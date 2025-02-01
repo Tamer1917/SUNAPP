7acafbcde57e63963e9c92f4e9c53dabb8f52606
Tamer1917 <tamer.j.m.manssour@gmail.com>
2025-02-01 06:34:47 +0300

    Add files via upload

---
+++ chess/firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+// تأكد من استبدال القيم بالقيم الخاصة بمشروعك من Firebase Console
+const firebaseConfig = {
+  apiKey: "AIzaSyBrfHwGulQyWW36LodXqNbcPtvV2J1wk8U",
+  authDomain: "sunapp-85501.firebaseapp.com",
+  projectId: "sunapp-85501",
+  storageBucket: "sunapp-85501.firebasestorage.app",
+  messagingSenderId: "146439638941",
+  appId: "1:146439638941:web:abef499250246650c6e974"
+  };
+  
+  // تهيئة Firebase
+  firebase.initializeApp(firebaseConfig);
+  
